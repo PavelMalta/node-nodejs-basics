c2a49003fc2eafc66dd9b432c6cb1424dc0f8322
Pavel Shamashov <shamashov87@mail.ru>
2023-07-06 12:18:00 -0400

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import fs from 'fs';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
+
+    const folderPath = 'src/fs/files';
+    const filePath = path.join(folderPath, 'fresh.txt');
+
+    try {
+        const fileExists = await fs.promises.access(filePath, fs.constants.F_OK)
+            .then(() => true)
+            .catch(() => false);
+
+        if (fileExists) {
+            throw new Error('File already exists.')
+        }
+
+        await fs.promises.mkdir(folderPath, { recursive: true });
+        await fs.promises.writeFile(filePath, 'I am fresh and young');
+        console.log('Fie created successfully!');
+    } catch (error) {
+        throw new Error(`FS operation failed. ${error.message}`);
+    }
 };
 
-await create();
+await create().catch(err => {
+    console.log(err.message);
+});

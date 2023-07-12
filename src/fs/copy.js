f2498f1a0f461c5f546860424808f997b3a08dd8
Pavel Shamashov <shamashov87@mail.ru>
2023-07-12 12:30:24 -0400

    feat: add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,43 @@
+import path from 'path';
+import fs from 'fs';
+
 const copy = async () => {
-    // Write your code here 
+    const folderPath = 'src/fs/files1';
+    const destinationPath = path.join(path.dirname(folderPath), 'files_copy');
+
+    try {
+        const mainFolderExists = await fs.promises.access(folderPath, fs.constants.F_OK)
+            .then(() => true)
+            .catch(() => false);
+        const destinationExists = await fs.promises.access(destinationPath, fs.constants.F_OK)
+            .then(() => true)
+            .catch(() => false);
+
+        if (!mainFolderExists) {
+            throw new Error('The folder being copied does not exist');
+        }
+
+        if (destinationExists) {
+            throw new Error('Destination folder already exists');
+        }
+
+        await fs.promises.mkdir(destinationPath);
+        const files = await fs.promises.readdir(folderPath);
+
+        for (const file of files) {
+            const sourceFile = path.join(folderPath, file);
+            const destinationFile = path.join(destinationPath, file);
+
+            await fs.promises.copyFile(sourceFile, destinationFile);
+        }
+
+        console.log('Folder copied successfully!');
+
+    } catch (err) {
+        throw new Error ('FS operation failed: ' + err.message);
+    }
 };
 
-await copy();
+await copy().catch(err => {
+    console.log(err.message);
+});
